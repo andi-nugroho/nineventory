e59d21f4290cd05e22ac32035c29a3935e5d9e15
tquila <nugrohoandi343@gmail.com>
2026-02-02 09:21:30 +0700

    Update file paths in dashboard.php

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../config/app.php';
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/config/app.php';
+require_once __DIR__ . '/config/database.php';
 
 use Nineventory\Auth;
 use Nineventory\Inventory;
